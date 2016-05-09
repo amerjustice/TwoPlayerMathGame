a3c3156f07706c38c9231d7657edc2b9175fd8f5
amerjustice <amerjustice@outlook.com>
2016-05-09 13:39:52 -0700

    Initial Commit

---
+++ TwoPlayerMathGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TwoPlayerMathGame
+//
+//  Created by Susan Justice on 2016-05-09.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwoPlayerMathGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwoPlayerMathGame
+//
+//  Created by Susan Justice on 2016-05-09.
+//  Copyright © 2016 Amer Justice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
