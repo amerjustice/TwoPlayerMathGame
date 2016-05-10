ccd766ce04d55ba6cab68bea4ca2ca5dd8a7af64
amerjustice <amerjustice@outlook.com>
2016-05-09 21:15:51 -0700

    added more UI elements

--- TwoPlayerMathGame/GameModel.h
+++ TwoPlayerMathGame/GameModel.h
@@ -32,4 +32,6 @@
 -(BOOL)endGame;
 
 
+
+
 @end
--- TwoPlayerMathGame/ViewController.m
+++ TwoPlayerMathGame/ViewController.m
@@ -18,6 +18,10 @@
 
 @property (nonatomic) NSMutableString* input;
 
+@property (weak, nonatomic) IBOutlet UILabel *correctLabel;
+
+
+
 
 @end
 
@@ -26,8 +30,8 @@
 
 -(void)updateUI {
     self.equation.text = [NSString stringWithFormat:@"%d + %d = %@", self.gameModel.num1, self.gameModel.num2, self.input];
-    self.player1Lives.text = [NSString stringWithFormat:@"Player 1: %d", self.gameModel.player1.lives];
-    self.player2Lives.text = [NSString stringWithFormat:@"Player 2: %d", self.gameModel.player2.lives];
+    self.player1Lives.text = [NSString stringWithFormat:@"Player 1 \u2764: %d", self.gameModel.player1.lives];
+    self.player2Lives.text = [NSString stringWithFormat:@"Player 2 \u2764: %d", self.gameModel.player2.lives];
 }
 
 - (IBAction)button1:(id)sender {
@@ -80,40 +84,97 @@
     [self updateUI];
 }
 
+- (IBAction)clearButton:(id)sender {
+    
+    [self.input setString:@""];
+    [self updateUI];
+}
+
+
 - (IBAction)buttonEnter:(id)sender {
     
     NSInteger userInput = [self.input integerValue];
     
     if ([self.gameModel answerIsCorrect:userInput]) {
         NSLog(@"Correct answer");
+
+        
+        self.correctLabel.textColor = [UIColor greenColor];
+        self.correctLabel.text = @"Correct";
+        self.correctLabel.alpha = 1.0;
+        [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveEaseIn
+                         animations:^{ self.correctLabel.alpha = 0.0;}
+                         completion:nil];
         
         
     } else {
         
         [self.gameModel currentPlayerAnsweredWrong];
         NSLog(@"incorrect answer");
+    
+        
+        self.correctLabel.textColor = [UIColor redColor];
+        self.correctLabel.text = @"Incorrect";
+        self.correctLabel.alpha = 1.0;
+        [UIView animateWithDuration:1 delay:0 options:UIViewAnimationOptionCurveEaseIn
+                         animations:^{ self.correctLabel.alpha = 0.0;}
+                         completion:nil];
     }
     
     
     
-    if([self.gameModel endGame]){
-        
-        self.gameModel = [[GameModel alloc] init];
-        self.input = [[NSMutableString alloc] init];
-        
-        
-    } else {
-        
+    if(!self.gameModel.endGame){
         [self.gameModel NextTurn];
         [self.gameModel nextQuestion];
         self.input = [[NSMutableString alloc] init];
         
+    
+    }else {
+        
+        NSString *whoLost;
+        
+        if([self.gameModel.currentPlayer isEqual:self.gameModel.player1]){
+            whoLost = @"Player 1 has lost\nWould you like to play again?";
+        } else if([self.gameModel.currentPlayer isEqual:self.gameModel.player2]){
+            whoLost = @"Player 2 has lost\nWould you like to play again?";
+        }
+        
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Game Over!"
+                                                                       message:whoLost
+                                                                preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction* yes = [UIAlertAction
+                              actionWithTitle:@"Yes"
+                              style:UIAlertActionStyleDefault
+                              handler:^(UIAlertAction * action)
+                              {
+                                  
+                                  self.gameModel = [[GameModel alloc]init];
+                                  [self updateUI];
+                                  
+                              }];
+        [alert addAction:yes];
+        
+        UIAlertAction* no = [UIAlertAction
+                             actionWithTitle:@"No"
+                             style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction * action)
+                             {
+                                 
+                             }];
+        [alert addAction:no];
+        
+        [self presentViewController:alert animated:YES completion:nil];
     }
     
-    
     [self updateUI];
-    
 }
+
+    
+
+
+    
+
 
 
 
